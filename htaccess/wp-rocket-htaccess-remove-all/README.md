70fac6cee70da5a1862210fe1f8b85a977cf1a14
Caspar Hübinger <caspar@glueckpress.com>
2018-02-23 14:37:54 +0100

    new plugin: remove all .htaccess rules

---
+++ htaccess/wp-rocket-htaccess-remove-all/README.md
@@ -0,0 +1,17 @@
+# WP Rocket | Remove All .htaccess Rules
+
+Removes all of WP Rocket’s rules from .htaccess (e.g. in order to fix an error 500).
+
+📝&#160;&#160;**Manual code edit recommended!**
+
+You can install and activate this plugin as it is, however, you probably won’t have to keep all of .htaccess WP Rocket’s .htaccess rules removed, but only one or two. Follow the [documentation](https://docs.wp-rocket.me/article/110-resolve-500-internal-server-error) to find out which ones.
+
+Documentation:
+* [Resolve 500 Internal Server Error](https://docs.wp-rocket.me/article/110-resolve-500-internal-server-error)
+
+To be used with:
+* any Apache setup where an error 500 appears, or where WP Rocket’s .htaccess rules can cause issues in the individual environment
+
+Last tested with:
+* WP Rocket 2.11.x
+* WordPress 4.9.x
---
+++ htaccess/wp-rocket-htaccess-remove-all/wp-rocket-htaccess-remove-all.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Plugin Name: WP Rocket | Remove All .htacces Rules
+ * Description: Removes all of WP Rocket’s rules from .htaccess (e.g. in order to fix an error 500).
+ * Plugin URI:  https://github.com/wp-media/wp-rocket-helpers/tree/master/htaccess/wp-rocket-htaccess-remove-all/
+ * Author:      WP Rocket Support Team
+ * Author URI:  http://wp-rocket.me/
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright SAS WP MEDIA 2018
+ */
+
+namespace WP_Rocket\Helpers\htaccess\remove_all;
+
+// Standard plugin security, keep this line in place.
+defined( 'ABSPATH' ) or die();
+
+
+// Remove rewrite rules block of WP Rocket from .htaccess.
+add_filter('rocket_htaccess_charset', '__return_false');
+add_filter('rocket_htaccess_etag', '__return_false');
+add_filter('rocket_htaccess_web_fonts_access', '__return_false');
+add_filter('rocket_htaccess_files_match', '__return_false');
+add_filter('rocket_htaccess_mod_expires', '__return_false');
+add_filter('rocket_htaccess_mod_deflate', '__return_false');
+add_filter('rocket_htaccess_mod_rewrite', '__return_false');
+
+
+/**
+ * Updates .htaccess, regenerates WP Rocket config file.
+ *
+ * @author Caspar Hübinger
+ */
+function flush_wp_rocket() {
+
+	if ( ! function_exists( 'flush_rocket_htaccess' )
+	  || ! function_exists( 'rocket_generate_config_file' ) ) {
+		return false;
+	}
+
+	// Update WP Rocket .htaccess rules.
+	flush_rocket_htaccess();
+
+	// Regenerate WP Rocket config file.
+	rocket_generate_config_file();
+}
+register_activation_hook( __FILE__, __NAMESPACE__ . '\flush_wp_rocket' );
+
+/**
+ * Removes customizations, updates .htaccess, regenerates config file.
+ *
+ * @author Caspar Hübinger
+ */
+function deactivate() {
+
+	// Remove all functionality added above.
+	remove_filter('rocket_htaccess_charset', '__return_false');
+	remove_filter('rocket_htaccess_etag', '__return_false');
+	remove_filter('rocket_htaccess_web_fonts_access', '__return_false');
+	remove_filter('rocket_htaccess_files_match', '__return_false');
+	remove_filter('rocket_htaccess_mod_expires', '__return_false');
+	remove_filter('rocket_htaccess_mod_deflate', '__return_false');
+	remove_filter('rocket_htaccess_mod_rewrite', '__return_false');
+
+	// Flush .htaccess rules, and regenerate WP Rocket config file.
+	flush_wp_rocket();
+}
+register_deactivation_hook( __FILE__, __NAMESPACE__ . '\deactivate' );

d9a7cc2fe5cfedd4fd040fb2243fb808c8eef583
Arun Basil Lal <arunbasillal@gmail.com>
2018-04-28 00:01:04 +0530

    New helper plugin: Disable Page Caching For Specific Pages

---
+++ cache/wp-rocket-no-cache-for-page/README.md
@@ -0,0 +1,14 @@
+# WP Rocket | Disable Page Caching For Specific Pages
+
+Disables WP Rocket’s page cache file generation on specific pages while preserving other optimization features.
+
+ðŸ“ **Manual code edit required before use!**
+
+Search for EDIT THIS and add the page ID's you want to exclude to the array as detailed in the inline comment.
+
+To be used with:
+* any setup
+
+Last tested with:
+* WP Rocket 3.0.x
+* WordPress 4.9.x
---
+++ cache/wp-rocket-no-cache-for-page/wp-rocket-no-cache-for-page.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Plugin Name: WP Rocket | Disable Page Caching For Specific Pages
+ * Description: Disables WP Rocket’s page cache file generation on specific pages while preserving other optimization features.
+ * Plugin URI:  https://github.com/wp-media/wp-rocket-helpers/tree/master/cache/wp-rocket-no-cache-for-page/
+ * Author:      WP Rocket Support Team
+ * Author URI:  https://wp-rocket.me/
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright SAS WP MEDIA 2018
+ */
+
+namespace WP_Rocket\Helpers\cache\no_cache_for_page;
+
+// Standard plugin security, keep this line in place.
+defined( 'ABSPATH' ) or die();
+
+/**
+ * Disable cache file generation on specific pages
+ * 
+ * @author Arun Basil Lal
+ */
+function no_cache_for_page( $filter ) {
+	
+	// EDIT THIS:
+	
+	/**
+	 * Change the ID (2 in this example) to the page ID you want to exclude.
+	 * For multiple ID's add all of them to the array as a comma seperated list. eg: array( 2, 4, 8 )
+	 **/
+	if ( function_exists( 'is_page' ) && ! is_page( array( 2 ) ) ) {
+		return $filter;
+	}
+	
+	// STOP EDITING
+	
+	return false;
+}
+add_filter( 'do_rocket_generate_caching_files', __NAMESPACE__ . '\no_cache_for_page' );
+
+/**
+ * Cleans entire cache folder on activation.
+ *
+ * @author Arun Basil Lal
+ */
+function clean_wp_rocket_cache() {
+
+	if ( ! function_exists( 'rocket_clean_domain' ) ) {
+		return false;
+	}
+
+	// Purge entire WP Rocket cache.
+	rocket_clean_domain();
+}
+register_activation_hook( __FILE__, __NAMESPACE__ . '\clean_wp_rocket_cache' );

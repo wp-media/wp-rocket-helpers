5a069b89a13425c149a6114db0ebab4ba59c3294
Arun Basil Lal <arunbasillal@gmail.com>
2018-11-21 11:03:58 +0100

    New helper - WP Rocket | Remove Plugin Conflict Warning

---
+++ various/wp-rocket-remove-plugin-conflict-warning/wp-rocket-remove-plugin-conflict-warning.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * Plugin Name: WP Rocket | Remove Plugin Conflict Warning
+ * Plugin URI: https://github.com/wp-media/wp-rocket-helpers/tree/master/various/wp-rocket-remove-plugin-conflict-warning
+ * Description: Remove the admin notice that warns about plugins that might conflict with WP Rocket. 
+ * Version: 1.0
+ * Author: WP Rocket Support Team
+ * Author URI: https://wp-rocket.me
+ * License:	GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright 2018 WP Media <support@wp-rocket.me>
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit; 
+
+add_filter( 'rocket_plugins_to_deactivate', '__return_false' );

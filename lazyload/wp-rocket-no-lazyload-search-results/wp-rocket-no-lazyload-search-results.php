01204dd808b2949e65f1adc9b4bc1671a5688dc6
Arun Basil Lal <arunbasillal@gmail.com>
2018-12-31 16:48:23 +0530

    WP Rocket | No LazyLoad for Search Results

---
+++ lazyload/wp-rocket-no-lazyload-search-results/wp-rocket-no-lazyload-search-results.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: WP Rocket | No LazyLoad for Search Results
+ * Description: Disables LazyLoad for search results
+ * Plugin URI:  https://github.com/wp-media/wp-rocket-helpers/tree/master/lazyload/wp-rocket-no-lazyload/
+ * Author:      WP Rocket Support Team
+ * Author URI:  http://wp-rocket.me/
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright SAS WP MEDIA 2018
+ */
+
+namespace WP_Rocket\Helpers\lazyload\no_lazyload_search_results;
+
+// Standard plugin security, keep this line in place.
+defined( 'ABSPATH' ) or die();
+
+/**
+ * Disable LazyLoad on search result views.
+ *
+ * @author Arun Basil Lal
+ */
+function deactivate_on_search_results() {
+
+	// Disable LazyLoad for images on a search results template.
+	if ( is_search() ) {
+		remove_action( 'wp_footer', 'rocket_lazyload_script', PHP_INT_MAX );
+	}
+}
+add_action( 'wp', __NAMESPACE__ . '\deactivate_on_search_results' );

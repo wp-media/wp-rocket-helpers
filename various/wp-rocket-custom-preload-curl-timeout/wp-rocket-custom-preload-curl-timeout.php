538812dd4f2a1005bd3af54e7eb2923a4670896d
Arun Basil Lal <arunbasillal@gmail.com>
2019-02-02 02:24:33 +0800

    Custom cURL Timeout - updated for rocket_partial_preload_url_request_args filter

--- various/wp-rocket-custom-preload-curl-timeout/wp-rocket-custom-preload-curl-timeout.php
+++ various/wp-rocket-custom-preload-curl-timeout/wp-rocket-custom-preload-curl-timeout.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: WP Rocket | Custom cURL Timeout For Sitemap Preload Request
- * Description: Increase the timeout for cURL from 5 seconds for sitemap preload request.
+ * Plugin Name: WP Rocket | Custom cURL Timeout For Preload Requests
+ * Description: Increase the timeout for cURL from 5 seconds for preload requests.
  * Plugin URI:  https://github.com/wp-media/wp-rocket-helpers/tree/master/various/wp-rocket-custom-preload-curl-timeout
  * Author:      WP Rocket Support Team
  * Author URI:  http://wp-rocket.me/
@@ -11,13 +11,13 @@
  * Copyright SAS WP MEDIA 2018
  */
 
-namespace WP_Rocket\Helpers\preload\sitemap_request_curl_timeout;
+namespace WP_Rocket\Helpers\preload\custom_request_curl_timeout;
 
 // Standard plugin security, keep this line in place.
 defined( 'ABSPATH' ) or die();
 
 /**
- * Increase cURL timeout for sitemap preload request. 
+ * Increase cURL timeout for preload request. 
  * 
  * @link https://github.com/wp-media/wp-rocket/blob/5caf48cafa91dec035c51264bc0aef6aae84112c/inc/functions/bots.php#L196-L207
  * @link https://codex.wordpress.org/Function_Reference/wp_remote_get
@@ -26,9 +26,18 @@ defined( 'ABSPATH' ) or die();
  */
 function custom_curl_timeout( $args ) {
 	
+	/** 
+	 * Making sure that the timeout set here is not changed.
+	 * https://github.com/wp-media/wp-rocket/blob/82680e5c8884c4c08bf547f4d4802d4b7d8cb93f/inc/classes/preload/class-partial-process.php#L54
+	 */
+	if ( isset( $args['timeout'] ) && ( $args['timeout'] === 0.01 ) ) {
+		return $args;
+	}
+	
 	// EDIT HERE: Specify the timeout in seconds. Default is 5 seconds. 
-	$args['timeout'] = 15;
+	$args['timeout'] = 30;
 	
 	return $args;
 }
 add_filter( 'rocket_preload_sitemap_request_args', __NAMESPACE__ . '\custom_curl_timeout' );
+add_filter( 'rocket_partial_preload_url_request_args', __NAMESPACE__ . '\custom_curl_timeout' );

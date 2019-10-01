56d9fc8ab2d5d7c5d6730ea7f99046db4d8c8b78
Vasilis Manthos <vmanthos@users.noreply.github.com>
2019-10-01 17:02:11 -0500

    Upload the WP Rocket | Enable Native Lazyload helper

---
+++ lazyload/wp-rocket-enable-native-lazyload/wp-rocket-enable-native-lazyload/README.md
@@ -0,0 +1,5 @@
+# WP Rocket | Enable Native Lazyload
+
+This helper plugin will enable native lazyload for browsers which support it.
+
+WP Rocket's lazyload feature will be used on browsers which don't support native lazyload.
---
+++ lazyload/wp-rocket-enable-native-lazyload/wp-rocket-enable-native-lazyload/wp-rocket-enable-native-lazyload.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name: WP Rocket | Enable Native Lazyload
+ * Description: Enables the application of native lazyload 
+ * Author:      WP Rocket Support Team
+ * Author URI:  http://wp-rocket.me/
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright SAS WP MEDIA 2019
+ */
+
+
+// Namespaces must be declared before any other declaration.
+namespace WP_Rocket\Helpers\lazyload\enable_native_lazyload;
+
+// Standard plugin security, keep this line in place.
+defined( 'ABSPATH' ) or die();
+
+/**
+ * Enable WP Rocket's lazyload feature
+ *
+ * @link filter: https://github.com/wp-media/wp-rocket/blob/163a03f4495a5d2e78c9822b9a3ac00f76485bd5/inc/classes/subscriber/Optimization/class-lazyload-subscriber.php#L158
+ * @author Vasilis Manthos
+ */
+ 
+add_filter( 'rocket_use_native_lazyload', __return_true );

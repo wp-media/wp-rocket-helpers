42ff0fb3237d3461ace71b958cfee5291118ef6e
Piotrek <piotr@wefoster.co>
2020-06-01 18:35:14 +0100

    Updated to also remove compression of static assets

--- htaccess/wp-rocket-htaccess-no-gzip/wp-rocket-htaccess-no-gzip.php
+++ htaccess/wp-rocket-htaccess-no-gzip/wp-rocket-htaccess-no-gzip.php
@@ -22,6 +22,7 @@ defined( 'ABSPATH' ) or die();
  * @link http://docs.wp-rocket.me/article/52-fix-for-weird-characters-displaying-on-your-web-page
  */
 add_filter( 'rocket_force_gzip_htaccess_rules', '__return_false' );
+add_filter('rocket_htaccess_compressed_assets', '__return_false');
 
 
 /**
@@ -53,6 +54,8 @@ function deactivate() {
 
 	// Remove all functionality added above.
 	remove_filter( 'rocket_force_gzip_htaccess_rules', '__return_false' );
+	remove_filter('rocket_htaccess_compressed_assets', '__return_false');
+
 
 	// Flush .htaccess rules, and regenerate WP Rocket config file.
 	flush_wp_rocket();

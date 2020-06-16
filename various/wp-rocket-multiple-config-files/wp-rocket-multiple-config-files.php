3e4b94f287c6425af550dd5233972e48b44a732c
Piotrek <piotr@wefoster.co>
2020-06-16 21:29:59 +0100

    Cache clearing update
    Added filter to make sure that the cache is cleared for all domains

--- various/wp-rocket-multiple-config-files/wp-rocket-multiple-config-files.php
+++ various/wp-rocket-multiple-config-files/wp-rocket-multiple-config-files.php
@@ -17,19 +17,33 @@ namespace WP_Rocket\Helpers\multiple_config_files;
 defined( 'ABSPATH' ) or die();
 
 
+function define_urls(){
+	
+	if ( ! is_array( $extra_url ) ) {
+        $extra_url = (array) $extra_url;
+    	}
+
+	// EDIT THIS. ADD ON EXTRA LINE PER DOMAIN
+	$extra_url[] = 'example.com';
+	//$extra_url[] = 'example.fr';
+	// STOP EDITING
+	
+	return $extra_url;
+}
+
 /**
  * Generate multiple config files, add each URL at the $extra_url[] array
  *
  */
 function clone_config( $config_files_path ) {
 
-	// EDIT THIS. ADD ON EXTRA LINE PER DOMAIN
+	if ( ! is_array( $config_files_path ) ) {
+        $config_files_path = (array) $config_files_path;
+    	}
+
+	$extra_urls = define_urls();
 	
-	$extra_url[] = 'another-url.com';
-	
-	// STOP EDITING
-	
-	foreach($extra_url as $url) {
+	foreach($extra_urls as $url) {
 
 		$sanitized_url =  preg_replace('#^https?://#', '', untrailingslashit( $url )).'.php' ;
 
@@ -42,9 +56,34 @@ function clone_config( $config_files_path ) {
 	return $config_files_path;
 
 }
-	
 add_filter( 'rocket_config_files_path', __NAMESPACE__ . '\clone_config' );
 
+
+/**
+ * Filter rocket_clean_domain_urls to include defined domains in cache purging
+ *
+ */
+function multi_domain_cache_clearing( $clean_domain_urls ){
+		
+	if ( ! is_array( $clean_domain_urls ) ) {
+        $clean_domain_urls = (array) $clean_domain_urls;
+   	}
+		
+	$extra_urls = define_urls();
+	
+	foreach($extra_urls as $url) {
+		
+		$url_with_scheme = rocket_add_url_protocol($url);
+		
+		$sanitized_url = untrailingslashit( $url_with_scheme );
+		
+		$clean_domain_urls[] = $sanitized_url;
+		
+	}
+	
+	return $clean_domain_urls;
+}
+add_filter( 'rocket_clean_domain_urls', __NAMESPACE__ . '\multi_domain_cache_clearing' );
 
 /**
  * Regenerates WP Rocket config file.
@@ -75,7 +114,8 @@ function deactivate() {
 	
 	// Remove customizations upon deactivation.
 	remove_filter( 'rocket_config_files_path', __NAMESPACE__ . '\clone_config' );
-	
+	remove_filter( 'rocket_clean_domain_urls', __NAMESPACE__ . '\multi_domain_cache_clearing' );
+
 	// Flush .htaccess rules, and regenerate WP Rocket config file.
 	flush_wp_rocket();
 

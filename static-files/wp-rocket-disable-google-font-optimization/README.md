5a0314fd1d06ca7e9b4b720b96c8b84a174efd31
Arun Basil Lal <arunbasillal@gmail.com>
2020-08-19 20:09:47 +0530

    WP Rocket | Disable Google Font Optimization

---
+++ static-files/wp-rocket-disable-google-font-optimization/README.md
@@ -0,0 +1,13 @@
+# WP Rocket | Enable SSL Cache
+
+Enables SSL cache so that WP Rocket caches pages with SSL.
+
+Documentation:
+* [{Docs title here}]({Docs URL here})
+
+To be used with:
+* Any setup where SSL cache is disabled. Since 3.3.4 SSL cache is enabled by default and the WP Rocket settings UI is removed. If SSL cache was disabled prior to 3.3.4, use this helper to enable it. 
+
+Last tested with:
+* WP Rocket {3.3.x}
+* WordPress {5.2.x}
---
+++ static-files/wp-rocket-disable-google-font-optimization/wp-rocket-disable-google-font-optimization.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name: WP Rocket | Disable Google Font Optimization
+ * Description: Disable Google Font Optimization in WP Rocket. To re-enable the option, visit Tools tab of WP Rocket. 
+ * Plugin URI:  https://github.com/wp-media/wp-rocket-helpers/tree/master/static-files/wp-rocket-disable-google-font-optimization
+ * Author:      WP Rocket Support Team
+ * Author URI:  http://wp-rocket.me/
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright SAS WP MEDIA 2020
+ */
+
+namespace WP_Rocket\Helpers\static_files\disable_google_font_optimization;
+
+// Standard plugin security, keep this line in place.
+defined( 'ABSPATH' ) or die();
+
+/**
+ * Disable Google Font Optimization in database.
+ *
+ * @author Arun Basil Lal
+ */
+function disable_google_font_optimization() {
+
+	if ( ! function_exists( 'update_rocket_option' ) || ! function_exists( 'rocket_clean_domain' ) ) {
+		return false;
+	}
+	
+	// Enable SSL Cache
+	update_rocket_option( 'minify_google_fonts', 0 );
+	
+	// Clear cache
+	rocket_clean_domain();
+}
+register_activation_hook( __FILE__, __NAMESPACE__ . '\disable_google_font_optimization' );

05fccbf8c7f543050cbe45b10dead05ff7154b37
Adame Dahmani <hello@adame.io>
2020-12-29 13:39:33 +0100

    Add WebP Target Attribute helper plugin

---
+++ various/wp-rocket-add-webp-target-attributes/README.md
@@ -0,0 +1,16 @@
+# WP Rocket | Add WebP Target Attribute
+
+Extends the attributes where WP Rocket does the WebP remplacement
+
+📝&#160;&#160;**Manual code edit required before use!**
+
+Add more attributes by adding more entries to the $attributes array. 
+
+Exemple: $attributes[] = 'data-thumb';
+
+To be used with:
+* any setup where WebP cache is enabled
+
+Last tested with:
+* WP Rocket 2.11.x
+* WordPress 4.8.x
---
+++ various/wp-rocket-add-webp-target-attributes/wp-rocket-add-webp-target-attributes.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: WP Rocket | Add WebP Target Attribute
+ * Description: Extends the attributes where WP Rocket does the WebP remplacement
+ * Author:      WP Rocket Support Team
+ * Author URI:  http://wp-rocket.me/
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright SAS WP MEDIA 2019
+ */
+
+
+// Namespaces must be declared before any other declaration.
+namespace WP_Rocket\Helpers\media\webp_attributes;
+
+// Standard plugin security, keep this line in place.
+defined( 'ABSPATH' ) or die();
+
+
+/**
+ * Extends the attributes where WP Rocket does the WebP remplacement.
+ *
+ * @param array $attributes Array holding the list of attributes to target
+ * @author Adame Dahmani
+ */
+
+
+function change_webp_attributes_array($attributes){
+	// the default array is ['href', 'src', 'srcset', 'content'];
+	$attributes[] = 'data-thumb';
+    return $attributes;
+}
+
+add_filter( 'rocket_attributes_for_webp', __NAMESPACE__ . '\change_webp_attributes_array' );

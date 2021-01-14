6903f7ee28b068ee1747ebf77deeacc53309cbd2
Vasilis Manthos <vmanthos@users.noreply.github.com>
2021-01-14 08:42:35 +0200

    Initial commit.

---
+++ compatibility/wp-rocket-static-exclude-opt-css-generatepress-woocommerce/README.md
@@ -0,0 +1,15 @@
+# WP Rocket | Exclude Files from Async CSS
+
+Exclude GeneratePress/WooCommerce CSS files from asynchronous loading - Optimize CSS Delivery.
+
+Documentation:
+* [GeneratePress and WooCommerce Issue](https://docs.wp-rocket.me/article/1399-generatepress-woocommerce)
+
+To be used with:
+* GeneratePress theme
+* WooCommerce
+* Optimize CSS Delivery and Mobile Caching
+
+Last tested with:
+* WP Rocket 4.8.2
+* WordPress 5.6
---
+++ compatibility/wp-rocket-static-exclude-opt-css-generatepress-woocommerce/wp-rocket-static-exclude-opt-css-generatepress-woocommerce.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Name: WP Rocket | Exclude Files from Async CSS
+ * Description: Exclude files from Optimize CSS Delivery option.
+ * Plugin URI:  https://github.com/wp-media/wp-rocket-helpers/tree/master/static/wp-rocket-static-exclude-opt-css/
+ * Author:      WP Rocket Support Team
+ * Author URI:  http://wp-rocket.me/
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright SAS WP MEDIA 2018
+ */
+
+namespace WP_Rocket\Helpers\static_files\exclude\optimized_css_GeneratePress_WooCommerce;
+
+// Standard plugin security, keep this line in place.
+defined( 'ABSPATH' ) or die();
+
+/**
+ * Exclude scripts from WP Rocket’s asnyc CSS option.
+ *
+ * @author Caspar Hübinger
+ * @param  array  $excluded_files   Array of script URLs to be excluded
+ * @return array                    Extended array script URLs to be excluded
+ */
+function exclude_files( $excluded_files = array() ) {
+
+	/**
+	 * EDIT THIS:
+	 * Replace, or multiply and edit below line as needed to exclude files.
+	 */
+	$excluded_files[] = '/wp-content/plugins/woocommerce/assets/css/woocommerce-smallscreen.css';
+	$excluded_files[] = '/wp-content/plugins/gp-premium/woocommerce/functions/css/woocommerce-mobile.min.css';
+
+
+	// STOP EDITING
+
+	return $excluded_files;
+}
+add_filter( 'rocket_exclude_async_css', __NAMESPACE__ . '\exclude_files' );

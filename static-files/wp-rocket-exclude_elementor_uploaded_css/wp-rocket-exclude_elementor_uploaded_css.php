b78fa771da3af098c086cb84c4fa2aa4f6312210
jorgemartine00 <jorgemartinezmenendez00@gmail.com>
2021-03-23 10:01:46 -0600

    Updated code
    Updated code to read and append uploads folder through hooks

--- static-files/wp-rocket-exclude_elementor_uploaded_css/wp-rocket-exclude_elementor_uploaded_css.php
+++ static-files/wp-rocket-exclude_elementor_uploaded_css/wp-rocket-exclude_elementor_uploaded_css.php
@@ -16,15 +16,25 @@ namespace WP_Rocket\Helpers\static_files\exclude\exclude_elementor_uploaded_css;
 defined( 'ABSPATH' ) or die();
 
 
-function get_elementor_uploaded_css($css_files = array()){
+function get_elementor_uploaded_css(){
+    $css_files = array();
     $uploads_dir = wp_upload_dir( null, false);
-    $css_uploads  =  scandir($uploads_dir["basedir"]."/elementor/css");
+    $uploads_path = "";
+    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN'){
+        $uploads_path = strval(str_replace (substr(str_replace("/","\\",get_home_path()), 0, -1),"",$uploads_dir["basedir"]));
+    } else {
+        $uploads_path = strval(str_replace (substr(get_home_path(), 0, -1),"",$uploads_dir["basedir"]));
+    } 
+    $css_uploads  =  scandir(wp_upload_dir( null, false)["basedir"]."/elementor/css");
     
     foreach($css_uploads as $css){
-        $file = pathinfo(scandir($uploads_dir["basedir"]."/elementor/css").$css);
-        if ($file["extension"] == "css" && $file["filename"] != "Arrayglobal"){
-            $css = "/wp-content/uploads/elementor/css/".$css;
-            array_push ( $css_files , $css );
+        $filepath = strval($uploads_dir["basedir"])."/elementor/css".$css;
+        $file = pathinfo($filepath);
+        if (array_key_exists("extension", $file) ){
+            if ($file["extension"] == "css" && $file["filename"] != "cssglobal"){      
+                $css = $uploads_path."/elementor/css/".$css;
+                array_push ( $css_files , $css );
+            }  
         }
     }
     return $css_files;

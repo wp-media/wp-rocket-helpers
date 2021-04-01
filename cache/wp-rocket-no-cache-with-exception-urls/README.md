1d3b1ad2937ff2d933863070273eec0af8cce1de
Natalia Drause <60236665+NataliaDrause@users.noreply.github.com>
2021-04-01 16:11:04 +0200

    add helper plugin files
    WP Rocket | Disable Page Caching with exception URLs

---
+++ cache/wp-rocket-no-cache-with-exception-urls/README.md
@@ -0,0 +1,18 @@
+# WP Rocket | Disable Page Caching with exception URLs
+
+Disables WP Rocket’s page cache while preserving other optimization features on all pages except for the selected URLs.
+
+🚧&#160;&#160;**ADVANCED CUSTOMIZATION, HANDLE WITH CARE!**
+📝 **Manual code edit required before use!**
+
+Edit lines 25-27. Replace the URLs, add additional lines by duplication for more URLs to exclude.
+
+Documentation:
+* [Disable Page Caching on all pages except for the selected URLs](https://docs.wp-rocket.me/article/1516-disable-page-caching-on-all-pages-except-for-the-selected-urls)
+
+To be used with:
+* any setup
+
+Last tested with:
+* WP Rocket 3.8.7
+* WordPress 5.7
---
+++ cache/wp-rocket-no-cache-with-exception-urls/wp-rocket-no-cache-with-exception-urls.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Plugin Name: WP Rocket | Disable Page Caching with exception URLs
+ * Description: Disables WP Rocket’s page cache while preserving other optimization features on all pages except for the selected URLs.
+ * Plugin URI:  https://github.com/wp-media/wp-rocket-helpers/tree/master/cache/wp-rocket-no-cache-with-exception-urls/
+ * Author:      WP Rocket Support Team
+ * Author URI:  http://wp-rocket.me/
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright SAS WP MEDIA 2021
+ */
+
+namespace WP_Rocket\Helpers\cache\no_cache_with_exception_urls;
+
+// Standard plugin security, keep this line in place.
+defined( 'ABSPATH' ) or die();
+
+/**
+ * Disable page caching in WP Rocket except for the selected URLs.
+ */
+$url =  "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+
+// You can add more URLs to this array as needed
+$excluded_url[] = 'example.com/page-one/';   // Replace the URL on this line.
+$excluded_url[] = 'example.com/page-two/';   // Remove this line or replace the URL on this line.
+$excluded_url[] = 'example.com/page-three/'; // Remove this line or replace the URL on this line.	Duplicate the line for more URLs to exclude.
+ 
+if ( ! in_array($url, $excluded_url)){
+	add_filter( 'do_rocket_generate_caching_files', '__return_false' );
+} 
+
+/**
+ * Cleans entire cache folder on activation.
+ *
+ * @author Arun Basil Lal
+ */
+function clean_wp_rocket_cache() {
+
+	if ( ! function_exists( 'rocket_clean_domain' ) ) {
+		return false;
+	}
+
+	// Purge entire WP Rocket cache.
+	rocket_clean_domain();
+}
+register_activation_hook( __FILE__, __NAMESPACE__ . '\clean_wp_rocket_cache' );

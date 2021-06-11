dc6b5101449328083511f07cd6119916ecf19fee
Sandy Figueroa <sandyfigueroa3@gmail.com>
2021-06-11 11:33:27 -0500

    Added 'WP Rocket | Image Dimensions for <picture> and external images' helper plugin

---
+++ various/wp-rocket-media-image-dimensions-picture-external-images/README.md
@@ -0,0 +1,14 @@
+# WP Rocket | Image Dimensions for <picture> and external images
+
+Disables / Enables Add Missing Image Dimensions for <picture> and external images
+
+📝&#160;&#160;**Manual code edit required before use!**
+
+Remove the "//" at the begining of the line for the feature you want to use.
+
+To be used with:
+* any setup where you want to disable or enable Add Missing Image Dimensions for <picture> and external images
+
+Last tested with:
+* WP Rocket 3.8.x
+* WordPress 4.8.x
---
+++ various/wp-rocket-media-image-dimensions-picture-external-images/wp-rocket-media-image-dimensions-picture-external-images.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: WP Rocket | Image Dimensions for <picture> and external images
+ * Description: Disables / Enables Add Missing Image Dimensions for <picture> and external images
+ * Author:      WP Rocket Support Team
+ * Author URI:  http://wp-rocket.me/
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright SAS WP MEDIA 2020
+ */
+
+namespace WP_Rocket\Helpers\media\image_dimensions;
+
+// Standard plugin security, keep this line in place.
+defined( 'ABSPATH' ) or die();
+
+
+/** Processes the "img" tag that's included in <picture> tags.
+ */
+//add_filter( 'rocket_specify_dimension_skip_pictures', '__return_false' );//If you want to use this feature remove the "//" at the beginning of the line.
+
+
+/** Enable setting image dimensions for external images.
+ */
+//apply_filters( 'rocket_specify_image_dimensions_for_distant', '__return_true' );//If you want to use this feature remove the "//" at the beginning of the line.

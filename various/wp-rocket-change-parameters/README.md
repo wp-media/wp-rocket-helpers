68aeb066ec8aed969ae178e43f05a7f0856dfb1d
Alfonso Catron <alfonso.catron@gmail.com>
2022-10-28 17:26:04 -0300

    new helper, WP Rocket | Change Parameters
    combines WP Rocket | Change Preload Parameters and WP Rocket | Change RUCSS Parameters in a single helper for faster setup

---
+++ various/wp-rocket-change-parameters/README.md
@@ -0,0 +1,32 @@
+# WP Rocket | Change Parameters
+
+Change "Preload" and "Remove Unused CSS" parameters to lower values, in order to reduce the CPU usage caused by these features on some servers.
+
+This helper can also be used to speed up the "Preload" and "Remove Unused CSS" processes on strong servers.
+
+📝&#160;&#160;**Manual code edit required before use!**
+
+Change: 
+
+- 1) PRELOAD BATCH SIZE: Line 28. Is the number of URLs that will be processed on each run. 45 is the default, this helper sets it to 30 URLs. Setting a lower value can help the server to work on fewer requests at a time.
+
+- 2) PRELOAD CRON INTERVAL: Line 46. Set the desired cron interval in seconds. It is the pause that will be applied between batches processing. By setting a higher value (default is 60 seconds), the server will have more time to rest between processing batches. The helper sets the value to 120 seconds.
+
+- 3) PRELOAD DELAY BETWEEN REQUESTS: Line 63. This is the pause between each request. A higher delay will reduce the CPU usage. Default is 0.5 seconds (500000 microseconds) the helper sets it to 0.6 seconds. You can use a value in seconds.
+
+- 4) RUCSS BATCH SIZE: Line 86, to set the batch size. It is the number of URLs that will be processed on each run. 100 is the default, this helper sets it to 50 URLs. Setting a lower value can help the server to work on fewer requests at a time.
+
+- 5) RUCSS CRON INTERVAL: Line 120, to set the desired cron interval in seconds. It is the pause that will be applied between batches processing. By setting a higher value (default is 60 seconds), the server will have more time to rest between processing batches. The helper sets the value to 120 seconds.
+
+
+Documentation:
+* [Change Preload Parameters](https://docs.wp-rocket.me/article/1715-customize-preload-parameters)
+* [Change Remove Unused CSS Parameters](https://docs.wp-rocket.me/article/1691-customize-remove-unused-css-parameters)
+* [High CPU usage](https://docs.wp-rocket.me/article/48-high-cpu-usage#remove-unused-css)
+
+To be used with:
+* Any setup where "Remove Unused CSS" or "Preload" are enabled and causing high CPU usage
+
+Last tested with:
+* WP Rocket 3.12.2.1
+* WordPress 6.x
---
+++ various/wp-rocket-change-parameters/wp-rocket-change-parameters.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Plugin Name: WP Rocket | Change Parameters
+ * Description: Reduce the CPU usage by changing the default Preload and RUCSS parameters
+ * Author:      WP Rocket Support Team
+ * Author URI:  http://wp-rocket.me/
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright SAS WP MEDIA 2022
+ */
+
+namespace WP_Rocket\Helpers\static_files\various\change_parameters;
+
+// Standard plugin security, keep this line in place.
+defined('ABSPATH') or die();
+
+
+/**
+ *  1) PRELOAD BATCH SIZE
+ *  Change the number of URLs to preload on each batch, 45 is the default.
+ *  A lower value can help the server to work on fewer requests at a time
+ */
+function preload_batch_size($value)
+{
+
+    // change this value, default is 45 urls:
+    $value = 35;
+
+    return $value;
+}
+
+add_filter('rocket_preload_cache_pending_jobs_cron_rows_count', __NAMESPACE__ . '\preload_batch_size');
+
+
+
+/**
+ *  2) PRELOAD CRON INTERVAL:
+ *  Set the desired cron interval in seconds
+ *  By setting a higher value the server will have more time to rest between processing batches.
+ */
+function preload_cron_interval($interval)
+{
+
+    // change this value, default is 60 seconds:
+    $interval = 120;
+
+    return $interval;
+}
+
+add_filter('rocket_preload_pending_jobs_cron_interval', __NAMESPACE__ . '\preload_cron_interval');
+
+
+
+/**
+ *  3) PRELOAD DELAY BETWEEN REQUESTS:
+ *  This is the delay between requests made to the same URL.
+ *  for example, for Separate cache files for mobile devices.
+ *  Default is 0.5 seconds (500000 microseconds)
+ */
+function preload_requests_delay($delay_between)
+{
+
+    // Edit this value, change the number of seconds
+    $seconds = 1;
+    // finish editing
+
+    // All done, don't change this part.
+    $delay_between = $seconds * 1000000;
+
+    return $delay_between;
+}
+
+add_filter('rocket_preload_delay_between_requests', __NAMESPACE__ . '\preload_requests_delay');
+
+
+/**
+ * 4) RUCSS BATCH SIZE
+ *  Change the processing batch value.
+ *  A lower value can help the server to work on fewer requests at a time
+ */
+function rucss_batch_size($rucss_batch_size)
+{
+
+    // change this value, default is 100 urls:
+    $rucss_batch_size = 50;
+
+    return $rucss_batch_size;
+}
+
+add_filter('rocket_rucss_pending_jobs_cron_rows_count', __NAMESPACE__ . '\rucss_batch_size');
+
+
+
+/**
+ *  5) RUCSS CRON Interval:
+ *  Set the desired cron interval in seconds
+ *  By setting a higher value the server will have more time to rest between processing batches.
+ */
+function rucss_cron_interval($cron_interval)
+{
+
+    // change this value, default is 60 seconds:
+    $cron_interval = 120;
+
+    return $cron_interval;
+}
+
+add_filter('rocket_rucss_pending_jobs_cron_interval', __NAMESPACE__ . '\rucss_cron_interval');

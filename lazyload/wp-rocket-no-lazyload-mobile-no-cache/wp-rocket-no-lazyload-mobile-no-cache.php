a153d4adb20af4d9a760d7068fc9549b3ba120d0
Natalia Drause <60236665+NataliaDrause@users.noreply.github.com>
2022-12-13 16:24:02 -0500

    update name of plugin

--- lazyload/wp-rocket-no-lazyload-mobile-no-cache/wp-rocket-no-lazyload-mobile-no-cache.php
+++ lazyload/wp-rocket-no-lazyload-mobile-no-cache/wp-rocket-no-lazyload-mobile-no-cache.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: WP Rocket | No LazyLoad for Mobile Devices (without Mobile Cache)
- * Description: Disables LazyLoad for mobile devices when Mobile Cache not active.
+ * Plugin Name: WP Rocket | No LazyLoad for Mobile Devices
+ * Description: Disables LazyLoad for mobile devices when Separate Cache for Mobile devices option is active.
  * Plugin URI:  https://github.com/wp-media/wp-rocket-helpers/tree/master/lazyload/wp-rocket-no-lazyload-mobile-no-cache/
  * Author:      WP Rocket Support Team
  * Author URI:  http://wp-rocket.me/

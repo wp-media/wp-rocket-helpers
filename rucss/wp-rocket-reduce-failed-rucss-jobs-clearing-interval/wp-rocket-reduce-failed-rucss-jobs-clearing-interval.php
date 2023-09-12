75bcc765f2c037395d679a1329b43ec8fde2d225
Joe DiSalvo <joedisalvo04@gmail.com>
2023-09-12 15:32:48 -0400

    updating default interval to 48 hours

--- rucss/wp-rocket-reduce-failed-rucss-jobs-clearing-interval/wp-rocket-reduce-failed-rucss-jobs-clearing-interval.php
+++ rucss/wp-rocket-reduce-failed-rucss-jobs-clearing-interval/wp-rocket-reduce-failed-rucss-jobs-clearing-interval.php
@@ -2,14 +2,14 @@
 
 /**
  * Plugin Name: WP Rocket | Reduce failed RUCSS jobs clearing interval
- * Description: Reduce failed RUCSS jobs clearing interval for cases that require less than the default 3 days.
+ * Description: Temporary fix only. Reduce failed RUCSS jobs clearing interval for cases that require less than the default 3 days.
  * Plugin URI:  
  * Author:      WP Rocket Support Team
  * Author URI:  http://wp-rocket.me/
  * License:     GNU General Public License v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
- * Copyright SAS WP MEDIA 2018
+ * Copyright SAS WP MEDIA 2023
  */
  
 namespace WP_Rocket\Helpers\rucss\change_failed_interval;
@@ -18,7 +18,7 @@ use WP_Rocket\Dependencies\Database\Query;
 
 add_filter('rocket_remove_rucss_failed_jobs_cron_interval', function () {
 	// START EDITING - HOURS IN SECONDS
-	$new_interval = 1 * 3600;
+	$new_interval = 48 * 3600;
 	// STOP EDITING
 	return $new_interval;
 }, 9999);

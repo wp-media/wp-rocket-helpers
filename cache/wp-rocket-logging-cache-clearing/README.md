a59773fcea5bfcfa387214b272fe7550062a899a
Mirna Freitez <mirnafreitez@gmail.com>
2023-11-30 15:27:25 -0300

    New helper to logs full and partial cache cleanup
    New helper to log full and partial cache cleanup

---
+++ cache/wp-rocket-logging-cache-clearing/README.md
@@ -0,0 +1,15 @@
+# WP Rocket | Logging Cache Clearing
+
+Creates two log files that record full and partial cache clears. 
+
+The files 01-full-cache-clear.txt and 02-partial-cache-clear.txt are created in the /wp-content/wpr-logs/ directory.
+
+Documentation:
+* [Cache is incomplete, cleared too frequently or randomly](https://docs.wp-rocket.me/article/1640-cache-is-incomplete-cleared-too-frequently-or-randomly)
+
+To be used with:
+* Any setup where it is suspected that the cache is being cleared too frequently or randomly without being triggered by the user or any of the actions described in [this document](https://docs.wp-rocket.me/article/78-how-often-is-the-cache-updated). 
+
+Last tested with:
+* WP Rocket 3.15.5
+* WordPress 6.0.1
---
+++ cache/wp-rocket-logging-cache-clearing/wp-rocket-logging-cache-clearing.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Plugin Name: WP Rocket | Logging cache clearing
+ * Description: Log the Full and Partial cache clearing
+ * Author:      WP Rocket Support Team
+ * Author URI:  http://wp-rocket.me/
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright SAS WP MEDIA 2018
+ */
+
+namespace WP_Rocket\Helpers\logs;
+
+// Standard plugin security, keep this line in place.
+defined( 'ABSPATH' ) or die();
+
+/**
+ * Logging cache clearing
+ *
+ */
+
+$logs_file_dir = '/wp-content/wpr-logs/';
+// create the uploads/wpr-logs folder if not exists.
+if (!file_exists($logs_file_dir)) {
+    wp_mkdir_p($logs_file_dir);
+}
+
+function generateCallTrace()
+{
+    $e = new \Exception();
+    $trace = explode("\n", $e->getTraceAsString());
+    // reverse array to make steps line up chronologically
+    $trace = array_reverse($trace);
+    array_shift($trace); // remove {main}
+        array_pop($trace); // remove call to this method
+        $length = count($trace);
+    $result = array();
+
+    for ($i = 0; $i < $length; $i++) {
+        $result[] = ($i + 1)  . ')' . substr($trace[$i], strpos($trace[$i], ' '));
+    }
+
+    return "" . implode("\n", $result);
+}
+
+/***************************************************************************************/
+				//FULL CACHE CLEARING
+/***************************************************************************************/
+add_action('before_rocket_clean_domain', function () {
+        error_log("\n\n*****************************************************************\n[" . date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']) . "]\n#CALLSTACK#####\n" . print_r(generateCallTrace(), true), 3, ABSPATH . "/wp-content/wpr-logs/01-full-cache-clear.txt");
+    });
+
+
+/***************************************************************************************/
+				//PARTIAL CACHE CLEARING
+/***************************************************************************************/
+function log_partialcacheclear($post, $purge_url, $lang) {
+        error_log("\n\n*****************************************************************\n[". date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']) . "]\n#CALLSTACK#####\n" . print_r(generateCallTrace(), true), 3, ABSPATH . "/wp-content/wpr-logs/02-partial-cache-clear.txt");
+       	error_log("\n\nPOST\n ID: " . print_r($post->ID, true), 3, ABSPATH . "/wp-content/wpr-logs/02-partial-cache-clear.txt");
+        error_log("\n URL: " . print_r(get_permalink($post->ID), true), 3, ABSPATH . "/wp-content/wpr-logs/02-partial-cache-clear.txt");
+        error_log("\n Post_type: " . print_r($post->post_type, true), 3, ABSPATH . "/wp-content/wpr-logs/02-partial-cache-clear.txt");
+
+        error_log("\n\n#PURGED_URLS#####\n" . print_r($purge_url, true), 3, ABSPATH . "/wp-content/wpr-logs/02-partial-cache-clear.txt");
+    }
+ add_action('after_rocket_clean_post', __NAMESPACE__ . '\log_partialcacheclear', 1000, 3);

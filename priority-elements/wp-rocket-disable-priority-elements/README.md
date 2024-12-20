67cc022411f6ca45b5699a2c1b4191942b4cd1f4
Alfonso Catron <alfonso.catron@gmail.com>
2024-12-20 11:15:51 -0300

    New helper plugin Disable Priority Elements

---
+++ priority-elements/wp-rocket-disable-priority-elements/README.md
@@ -0,0 +1,20 @@
+# WP Rocket | Clear Priority Elements of Specific Pages
+
+
+### 📝Manual code edit required before use!
+Please find the comment (// EDIT HERE) showing where to make changes.
+
+### Customize the following configs to your needs:
+- <code>clear_triggering_actions</code> - Speficy which actions should trigger clearing of Priority Elements.
+- <code>page_to_be_cleared_ids</code> - Specify the IDs of the pages whose Priority Elements should be cleared.
+
+### Documentation:
+https://docs.wp-rocket.me/article/1841-priority-elements
+
+### To be used with:
+* Any setup where Priority Elements of specific pages need to be cleared when specific actions occur.
+
+
+### Last tested with:
+* WP Rocket 3.17.2
+* WordPress 6.7.x
---
+++ priority-elements/wp-rocket-disable-priority-elements/wp-rocket-disable-priority-elements.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Plugin Name: WP Rocket | Disable Priority Elements
+ * Description: Disable Priority Elements, including Automatic Lazy Rendering and Optimize Critical Images.
+ * Plugin URI:  https://docs.wp-rocket.me/article/1841-priority-elements
+ * Author:      WP Rocket Support Team
+ * Author URI:  http://wp-rocket.me/
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright SAS WP MEDIA 2024
+ */
+
+namespace WP_Rocket\Helpers\disable_priority_elements;
+
+// Standard plugin security, keep this line in place.
+defined( 'ABSPATH' ) or die();
+
+// Disable Optimize Critical Images
+add_filter( 'rocket_above_the_fold_optimization', '__return_false', 999 );
+
+// Disable Automatic Lazy Render
+add_filter( 'rocket_lrc_optimization', '__return_false', 999 );
+
+// Clear cache on activation 
+register_activation_hook(__FILE__, __NAMESPACE__ .'\prepare_things_upon_activation');
+
+function prepare_things_upon_activation() {
+
+  // Clear domain cache
+  if ( function_exists( 'rocket_clean_domain' ) ) {
+    rocket_clean_domain();
+  }
+}
+
+// Clear cache on deactivation 
+register_deactivation_hook( __FILE__ , __NAMESPACE__ . '\deactivate_plugin' );
+
+function deactivate_plugin() {
+
+  // Clear domain cache
+  if ( function_exists( 'rocket_clean_domain' ) ) {
+    rocket_clean_domain();
+  }
+}

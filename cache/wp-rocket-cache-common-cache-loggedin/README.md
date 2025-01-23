1149535e9612205fc1da77665a61bc029b460027
joejoe04 <45705494+joejoe04@users.noreply.github.com>
2025-01-23 14:30:07 -0500

    Account for user cache enabled disabled (#115)
    * update to ensure rocket_common_cache_logged_users is correctly added to and removed from rocket config file when needed
    
    * creating zip file
    
    * creating zip file

--- cache/wp-rocket-cache-common-cache-loggedin/README.md
+++ cache/wp-rocket-cache-common-cache-loggedin/README.md
@@ -1,13 +1,27 @@
 # WP Rocket | Common Cache For Logged-in Users
 
-Use a common cache for all logged-in users instead of creating a user specific cache. Note: Logged out users will have a different cache. 
+<br>
+
+Use a common cache for all logged-in users instead of creating a user specific cache. 
+
+<br>
+
+Notes:
+* Logged out users will have a different cache.
+* This helper will not do anything if WP Rocket's User Cache is not enabled.
+
+<br>
 
 Documentation:
 * [User Cache](https://docs.wp-rocket.me/article/313-user-cache)
 
+<br>
+
 To be used with:
 * Any setup where you want all logged in users to use the same cache.
 
+<br>
+
 Last tested with:
-* WP Rocket 3.12.x
-* WordPress 6.1.x
+* WP Rocket 3.18.x
+* WordPress 6.7.x
--- cache/wp-rocket-cache-common-cache-loggedin/wp-rocket-cache-common-cache-loggedin.php
+++ cache/wp-rocket-cache-common-cache-loggedin/wp-rocket-cache-common-cache-loggedin.php
@@ -8,7 +8,7 @@
  * License:     GNU General Public License v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
- * Copyright SAS WP MEDIA 2018
+ * Copyright SAS WP MEDIA 2024
  */
 
 namespace WP_Rocket\Helpers\common_cache_loggedin;
@@ -24,7 +24,30 @@ defined( 'ABSPATH' ) or die();
  * 
  * @link https://github.com/wp-media/wp-rocket/blob/33578f0dd568819d3e5912e38cafb9d51d5964d7/inc/functions/files.php#L98-L100
  */
-add_filter( 'rocket_common_cache_logged_users', '__return_true' );
+add_action( 'update_option', function( $option_name, $old_value, $value ) {
+
+	if ( $option_name !== 'wp_rocket_settings' ) { 
+		return;
+	}
+
+	if ( $value['cache_logged_user'] ) {
+		add_filter( 'rocket_common_cache_logged_users', '__return_true' );
+	}
+	else {
+		remove_filter( 'rocket_common_cache_logged_users', '__return_true' );
+	}
+}, 9999, 3 );
+
+
+// We also need to account for when WP Rocket is activated/deactivated while this helper plugin is activated
+// The update_option hook won't fire in this case, so we check for the option directly
+if ( get_option( 'wp_rocket_settings', [] )['cache_logged_user'] ) {
+	add_filter( 'rocket_common_cache_logged_users', '__return_true' );
+}
+else {
+	remove_filter( 'rocket_common_cache_logged_users', '__return_true' );
+}
+
 
 /**
  * Regenerates WP Rocket config file.
@@ -48,6 +71,10 @@ register_activation_hook( __FILE__, __NAMESPACE__ . '\flush_wp_rocket' );
  * @author Arun Basil Lal
  */
 function deactivate() {
+
+	if ( ! is_plugin_active( 'wp-rocket/wp-rocket.php' ) ) {
+		return false;
+	}
 
 	// Remove the filter to enable common cache for logged-in users. 
 	remove_filter( 'rocket_common_cache_logged_users', '__return_true' );

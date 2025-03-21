b49010035ed9056a057129b49f850224c928de0d
Suzan E <30945117+suzoutlet@users.noreply.github.com>
2025-03-21 10:04:33 +0300

    Updated Helper's name

---
+++ static-files/wp-rocket-change-delay-js-execution-script/README.md
@@ -0,0 +1,21 @@
+# WP Rocket | Change Delay JS Execution Script
+
+Change the Delay JavaScript Execution Feature's Script 
+
+Documenetation
+- [Troubleshoot Delay JavaScript Execution Issues | Common Issues](https://docs.wp-rocket.me/article/1655-troubleshoot-delay-javascript-execution-issues#common-issues).
+
+On plugin activation/deactivation:
+* The cache is cleared.
+* Delay JavaScript Execution Feature's default script version '2.0.3' is changed to '1.2.6' or the specified version.
+
+
+To be used with:
+
+- Any setup where Delay JavaScript Execution feature causes a regression and script version needs to be downgraded. By default the script version is set to '2.0.3' but this can be changed to previous version. For now it is only possible to change to '1.2.6'.
+- WP Rocket version **3.18.3** and newer.
+
+Last tested with:
+
+- WP Rocket {3.18.3}
+- WordPress {6.7.x}
--- static-files/wp-rocket-change-delay-js-execution-script/wp-rocket-change-delay-js-execution-script.php
+++ static-files/wp-rocket-change-delay-js-execution-script/wp-rocket-change-delay-js-execution-script.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Plugin Name: WP Rocket | Downgrade Delay JS Execution Script
+ * Plugin Name: WP Rocket | Change Delay JS Execution Script
  * Plugin URI: https://github.com/wp-media/wp-rocket-helpers/tree/master/static-files/wp-rocket-downgrade-delay-js-script/
  * Author:      WP Rocket Support Team
- * Description: Downgrade the Delay JavaScript Execution Feature's Script
+ * Description: Change the Delay JavaScript Execution Feature's Script
  * Author URI:  http://wp-rocket.me/
  * License:     GNU General Public License v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -11,12 +11,12 @@
  * Copyright SAS WP MEDIA 2025
  */
 
-namespace WP_Rocket\Helpers\static_files\downgrade_delay_js_script;
+namespace WP_Rocket\Helpers\static_files\change_delay_js_script;
 
 // Standard plugin security, keep this line in place.
 defined( 'ABSPATH' ) or die();
  
-//For now it is only possible to downgrade to '1.2.6'. Default value is 2.0.3
+//For now it is only possible to change to '1.2.6'. Default value is '2.0.3'
 add_filter( 'rocket_delay_js_version_js_script', function () {
   return '1.2.6';
   });

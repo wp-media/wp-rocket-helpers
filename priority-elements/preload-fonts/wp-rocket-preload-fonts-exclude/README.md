086e71981c72a906de5380d895fce5c76032e361
Alfonso Catron <alfonso.catron@gmail.com>
2025-06-05 13:20:28 -0300

    New helper WP Rocket | Exclude Fonts from Preload Fonts

---
+++ priority-elements/preload-fonts/wp-rocket-preload-fonts-exclude/README.md
@@ -0,0 +1,32 @@
+
+# WP Rocket | Exclude Fonts from Preload Fonts
+
+This helper plugin lets you exclude specific fonts from being preloaded by WP Rocket's Fonts Preload optimization: [WP Rocket: Preload Fonts](https://docs.wp-rocket.me/article/1317-preload-fonts)
+
+## How to edit
+To define the fonts you want to exclude:
+
+1. Open the plugin file.
+2. Find this section:
+
+```php
+// START editing — add full or partial font filenames to exclude as in the examples below
+// $exclusions[] = 'OpenSans.woff2';
+// $exclusions[] = 'my-font-subset.woff';
+// END editing
+```
+You can uncomment one of the examples or add one, per font you want to exclude:
+
+     $exclusions[] = 'your-font-file.woff2'; 
+
+3. Save the file, zip it
+4. Upload the plugin to your WordPress site and activate it   
+5. Clear the Priority Elements, WP Rocket will automatically apply the exclusions on the regeneration.
+
+**To undo the changes**
+1. Just deactivate the plugin from the Plugins menu. The exclusions will no longer apply.
+2. Clear the Priority Elements again
+
+## Last tested with:
+WP Rocket 3.19
+WordPress 6.8
---
+++ priority-elements/preload-fonts/wp-rocket-preload-fonts-exclude/wp-rocket-preload-fonts-exclude.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: WP Rocket | Exclude Fonts from Preload Fonts
+ * Description: Exclude specific fonts from WP Rocket's Fonts Preload feature using the `rocket_preload_fonts_excluded_fonts` filter.
+ * Author:      WP Rocket Support Team
+ * Author URI:  https://wp-rocket.me/
+ * License:     GNU General Public License v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Copyright SAS WP MEDIA 2025
+ */
+
+namespace WP_Rocket\Helpers\exclude_fonts_preload;
+
+// Standard plugin security
+defined( 'ABSPATH' ) or die();
+
+/**
+ * Exclude specific fonts from WP Rocket preload
+ *
+ * @param array $exclusions Fonts to exclude (e.g. ['font.woff2', 'OpenSans.woff']).
+ * @return array
+ */
+function wpr_exclude_fonts_from_preload( array $exclusions ): array {
+    // START editing — add full or partial font filenames to exclude
+    //$exclusions[] = 'OpenSans.woff2';
+    //$exclusions[] = 'my-font-subset.woff';
+    $exclusions[] = 'Manrope-VariableFont_wght.woff2';
+
+
+    // END editing
+    return $exclusions;
+}
+
+add_filter( 'rocket_preload_fonts_excluded_fonts', __NAMESPACE__ . '\wpr_exclude_fonts_from_preload' );
